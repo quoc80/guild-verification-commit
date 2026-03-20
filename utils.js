bdf9da13e3be722bbc6eef16494b2e1954f42066
pquoc <quocgm@gmail.com>
2026-03-20 10:28:50 +0700

    Add dark mode support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:28:34.859Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-20T03:28:49.060Z
+Commit: Add dark mode support
