102c00598c10ebab0801ba1c13a7a28e3640e3ac
pquoc <quocgm@gmail.com>
2026-03-20 10:25:40 +0700

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:23:40.421Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-20T03:25:39.027Z
+Commit: Resolve null pointer exception in user service
