80e967f55a005daef60b9589b7bf36cb273bd2f5
pquoc <quocgm@gmail.com>
2026-03-20 10:28:22 +0700

    Patch security vulnerability in authentication

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:28:14.164Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-20T03:28:20.979Z
+Commit: Patch security vulnerability in authentication
