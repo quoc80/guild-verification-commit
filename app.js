568dd3fb946d12609da0ee9881bc3947b0e20850
pquoc <quocgm@gmail.com>
2026-03-20 10:28:25 +0700

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:28:20.979Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-20T03:28:23.496Z
+Commit: Fix CSS styling bug on mobile devices
