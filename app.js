ac5e8814f23cdd58019f9a61b123f1f93799dc31
pquoc <quocgm@gmail.com>
2026-03-20 10:25:32 +0700

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:25:29.452Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-20T03:25:31.492Z
+Commit: Patch API endpoint error handling
