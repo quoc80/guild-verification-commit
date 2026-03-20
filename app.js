4485035f25983057e40c8b8d531426267ce10256
pquoc <quocgm@gmail.com>
2026-03-20 10:25:30 +0700

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:25:15.544Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-20T03:25:29.452Z
+Commit: Fix CSS styling bug on mobile devices
