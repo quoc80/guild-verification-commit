60a513fc64bc41175896f20cb3be8aa8127677f0
pquoc <quocgm@gmail.com>
2026-03-20 10:27:49 +0700

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T03:25:31.492Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-20T03:27:48.873Z
+Commit: Fix memory leak in event handlers
